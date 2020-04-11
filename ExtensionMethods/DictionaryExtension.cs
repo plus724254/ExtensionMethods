0ec7f06003135a9fa45fe4dc652ba91770432367
leo <plus724254@gmail.com>
2020-04-11 10:45:37 +0800

    First Commit

---
+++ ExtensionMethods/DictionaryExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HuashanCRM.Extension
+{
+    public static class DictionaryExtension
+    {
+        public static TValue GetValueOrDefaultValue<TKey, TValue> (this IDictionary<TKey, TValue> source, TKey key, TValue defaultValue = default(TValue))
+        {
+            TValue value; 
+            if(source.TryGetValue(key, out value))
+            {
+                return value;
+            }
+            else 
+            {
+                return defaultValue;
+            }
+        }
+    }
+}
