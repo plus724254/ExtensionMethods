452dedd71295939020add9faac6e93f5b6f0372e
Yi-Yong Chen <42492294+plus724254@users.noreply.github.com>
2022-05-13 12:41:04 +0800

    加入DbContextExtension

---
+++ ExtensionMethods/DbContextExtension.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace HuashanCRM.Extension
+{
+    public static class DbContextExtension
+    {
+        /// <summary>
+        /// 從本地快取移除實體
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="db"></param>
+        /// <param name="entity"></param>
+        public static void Detach<T>(this DbContext db, T entity) where T : class
+        {
+            var context = ((IObjectContextAdapter)db).ObjectContext;
+            context.Detach(entity);
+        }
+
+        /// <summary>
+        /// 取得實體的主鍵
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="db"></param>
+        /// <returns></returns>
+        public static IEnumerable<string> GetEntityKeys<T>(this DbContext db) where T : class
+        {
+            var context = ((IObjectContextAdapter)db).ObjectContext;
+            var keys = context.CreateObjectSet<T>().EntitySet.ElementType.KeyProperties.Select(x => x.Name);
+
+            return keys;
+        }
+
+        private static Expression<Func<T, bool>> GetFindExpression<T>(T entity, IEnumerable<string> keys) where T : class
+        {
+            var parameter = Expression.Parameter(typeof(T), "x");
+
+            var keyExpressions = keys.Select(x =>
+            {
+                var member = Expression.PropertyOrField(parameter, x);
+                var entityValue = typeof(T).GetProperty(x).GetValue(entity);
+                var equalExpression = Expression.Equal(member, Expression.Constant(entityValue));
+
+                return equalExpression;
+            }).ToList();
+
+            if (keys.Count() == 1)
+            {
+                return Expression.Lambda<Func<T, bool>>(keyExpressions[0], new[] { parameter });
+            }
+
+            var andExpression = Expression.AndAlso(keyExpressions[0], keyExpressions[1]);
+
+            for (var i = 2; i < keyExpressions.Count; i++)
+            {
+                andExpression = Expression.AndAlso(andExpression, keyExpressions[i]);
+            }
+            return Expression.Lambda<Func<T, bool>>(andExpression, new[] { parameter });
+        }
+
+        /// <summary>
+        /// 從本地快取尋找實體
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="db"></param>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public static T FindLocal<T>(this DbContext db, T entity) where T : class
+        {
+            var entityKeys = db.GetEntityKeys<T>();
+            var findExpression = GetFindExpression<T>(entity, entityKeys).Compile();
+
+            return db.Set<T>().Local.FirstOrDefault(findExpression);
+        }
+
+        /// <summary>
+        /// 若本地快取存在實體，將他移除快取
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="db"></param>
+        /// <param name="entity"></param>
+        public static void DetachWhenExist<T>(this DbContext db, T entity) where T : class
+        {
+            var localEntity = db.FindLocal(entity);
+            if (localEntity != null)
+            {
+                db.Detach(localEntity);
+            }
+        }
+    }
+}
