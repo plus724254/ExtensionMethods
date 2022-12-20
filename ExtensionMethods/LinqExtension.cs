36a6a86df058e2a88e050af358c76de87918a96f
Yi-Yong Chen <42492294+plus724254@users.noreply.github.com>
2022-12-20 16:56:27 +0800

    Update LinqExtension.cs
    加入 skipif takeif OrderByDescendingIf

--- ExtensionMethods/LinqExtension.cs
+++ ExtensionMethods/LinqExtension.cs
@@ -23,5 +23,45 @@ namespace HuashanCRM.Extension
             else
                 return source;
         }
+        
+        public static IQueryable<TSource> SkipIf<TSource>(this IQueryable<TSource> source, bool condition, int? skipValue)
+        {
+            if (condition)
+                return source.Skip(skipValue.Value);
+            else
+                return source;
+        }
+
+        public static IEnumerable<TSource> SkipIf<TSource>(this IEnumerable<TSource> source, bool condition, int? skipValue)
+        {
+            if (condition)
+                return source.Skip(skipValue.Value);
+            else
+                return source;
+        }
+
+        public static IQueryable<TSource> TakeIf<TSource>(this IQueryable<TSource> source, bool condition, int? takeValue)
+        {
+            if (condition)
+                return source.Take(takeValue.Value);
+            else
+                return source;
+        }
+
+        public static IEnumerable<TSource> TakeIf<TSource>(this IEnumerable<TSource> source, bool condition, int? takeValue)
+        {
+            if (condition)
+                return source.Take(takeValue.Value);
+            else
+                return source;
+        }
+
+        public static IQueryable<TSource> OrderByDescendingIf<TSource, TKey>(this IQueryable<TSource> source, bool condition, Expression<Func<TSource, TKey>> keySelector)
+        {
+            if (condition)
+                return source.OrderByDescending(keySelector).AsQueryable();
+            else
+                return source;
+        }
     }
 }
